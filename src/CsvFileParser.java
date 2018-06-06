f11242fbcb1f0b7057984c3eec41d461c6c8da1a
BenConrads <32313092+BenConrads@users.noreply.github.com>
2018-06-06 19:28:28 +0200

    Update CsvFileParser.java
    Obsługa błędów

--- src/CsvFileParser.java
+++ src/CsvFileParser.java
@@ -14,58 +14,53 @@ public class CsvFileParser implements Serializable {
     }
 
     public String readString() throws IOException {
-        BufferedReader in =
-                new BufferedReader(
-                        new FileReader(obiektIn));
-        char c;
-        StringBuffer strng = new StringBuffer();
-        while ((c = (char) in.read()) != (char) -1)//'\n')
-            strng.append(c);
-        strng.append('\n');
-        in.close();
-        stringMatrix = strng.toString();
+        try{
+            BufferedReader in =
+                    new BufferedReader(
+                            new FileReader(obiektIn));
+
+            char c;
+            StringBuffer strng = new StringBuffer();
+            while ((c = (char) in.read()) != (char) -1)
+                strng.append(c);
+            strng.append('\n');
+            in.close();
+            stringMatrix = strng.toString();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Nie można znaleźć pliku. " +
+                    "Została podana błędna nazwa lub plik nie znajduje się w folderze projektu.");
+
+        }
+
+
         return stringMatrix;
 
     }
 
-    public Matrix convertToMatrix(String sep) {
+    public Matrix convertToMatrix() {
         ArrayList<ArrayList<Double>> resultData = new ArrayList<ArrayList<Double>>();
-        String[] lines = stringMatrix.split("\n");
-        int i = 0;
-        for (String line : lines) {
-            String[] fields = line.split(sep);
-            resultData.add(new ArrayList<Double>());
-            int j = 0;
-            for (String field : fields) {
-                Double v = Double.parseDouble(field);
-                resultData.get(i).add(v);
-                j++;
+        try {
+            String[] lines = stringMatrix.split("\n");
+            int i = 0;
+            for (String line : lines) {
+                String[] fields = line.split(";");
+                resultData.add(new ArrayList<Double>());
+                int j = 0;
+                for (String field : fields) {
+                    Double v = Double.parseDouble(field);
+                    resultData.get(i).add(v);
+                    j++;
+                }
+                i++;
             }
-            i++;
+        }
+        catch (NullPointerException e){
+            return new Matrix(null);
         }
 
         return new Matrix(resultData);
     }
 
-    /*
-
-    public void FileOpenOut() throws IOException {
-        obiektOut = new ObjectOutputStream(
-                new FileOutputStream("abc.txt"));
-    }
-
-    public String getObiektIn() {
-        return obiektIn;
-    }
-
-
-    public void FileCloseOut() throws IOException {
-        obiektOut.close();
-    }
-
-    public ObjectOutputStream getObiektOut() {
-        return obiektOut;
-    }
-
-*/
 }
