438d4f3b1c444d2da365f16cd970c82acdfe0bd2
BenConrads <32313092+BenConrads@users.noreply.github.com>
2018-06-06 19:28:56 +0200

    Update Main.java
    Klasa main gotowa do działania

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,85 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import javax.swing.*;
 
 public class Main implements Serializable {
+
+    public static int[][] connectionsMatrix;
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        CsvFileParser inputFile = new CsvFileParser("SDG.csv");
-        System.out.println(inputFile.readString());
-
-        //CsvFileParser inputNames = new CsvFileParser("nazwy.csv");
-        //System.out.println(inputNames.readString());
 
 
-        Matrix X = inputFile.convertToMatrix(";");
+        String inputName = JOptionPane.showInputDialog("Podaj nazwę pliku csv zawierającą macierz danych:",
+                "FileName.csv");
+        /*String threshold = JOptionPane.showInputDialog("Podaj graniczną wartość wspólnych projektów.",
+                "liczba całkowita dodatnia");*/
 
 
-        Matrix Xt = X.transpose();
-        Matrix XtX = Xt.dotProduct(X);
-        X.display();
-        Xt.display();
-        XtX.display();
 
-        ArrayList<ArrayList<Integer>> connections = AdditionalFunctions.getConnectionLists(XtX, 3.0);
-        int projectNum = 1;
-        for (ArrayList<Integer> row : connections) {
-            System.out.print("Projekt nr " + projectNum + ": ");
-            for (Integer cell : row) {
-                System.out.print(cell + 1+ ", ");
+        CsvFileParser inputFile = new CsvFileParser(inputName);
+        if (inputFile.readString() != null) System.out.println(inputFile.readString());
+        else System.out.print("Nie udało się załadować pliku");
+
+
+
+
+        Matrix X = inputFile.convertToMatrix();
+
+        if (X.getData() != null){
+
+            Matrix Xt = X.transpose();
+            Matrix XtX = Xt.dotProduct(X);
+            System.out.println("Macierz wejściowa");
+            X.display();
+            System.out.println("Macierz przetransponowana");
+            Xt.display();
+            System.out.println("Macierz XtX");
+            XtX.display();
+
+            //System.out.println(XtX.getClass());
+            System.out.print("Macierz XtX ma wymiary " +XtX.nrow());
+            System.out.print(" na " + XtX.ncol() + ".\n");
+
+            connectionsMatrix = AdditionalFunctions.getConnections(XtX);
+
+            //System.out.println("stworzyłem macierz powiązań:");
+
+            int numOfProjects = connectionsMatrix.length;
+            //String[] projectNames = inputNames.readString().split("\n");
+
+
+            System.out.println("Powiązane projekty:");
+
+            for (int i = 0; i < numOfProjects; i++) {
+                ArrayList<Integer> connections = AdditionalFunctions.getStringForRow(connectionsMatrix, i);
+                System.out.print((i + 1) + " : ");
+                for (Integer connection : connections) {
+                    System.out.print(String.format("SDG%d, ", connection + 1));
+                }
+                System.out.print("\b\b\n");
             }
-            System.out.print("\n");
-            projectNum++;
-        }
-        System.out.print("\n");
-
-
-        ArrayList<ArrayList<Integer>> allGraphs = AdditionalFunctions.listAllFullGraphs(connections);
-        for (ArrayList<Integer> row : allGraphs) {
-            System.out.println(row);
-        }
-
-
-
- /*       System.out.println(XtX.getClass());
-        System.out.println(XtX.ncol());
-        System.out.println(XtX.nrow());
-*/
-    /*    int[][] connectionsMatrix = AdditionalFunctions.getConnections(XtX, 0.4);
-
-        //System.out.println("stworzyłem macierz powiązań:");
-
-        int numOfProjects = connectionsMatrix.length;
-        //String[] projectNames = inputNames.readString().split("\n");
-
-
-        System.out.println("Powiązane projekty:");
-
-        for (int i = 0; i < numOfProjects; i++) {
-            ArrayList<Integer> connections = AdditionalFunctions.getStringForRow(connectionsMatrix, i);
-            System.out.print((i + 1) + " : ");
-            for (Integer connection : connections) {
-                System.out.printf(String.format("SDG%d,", connection + 1));
+            System.out.print("Lista pełnych grafów: \n\n");
+            ArrayList<ArrayList<Integer>> fullGraphsList = AdditionalFunctions.listAllFullGraphs(X);
+            int fullGraphsListSize = fullGraphsList.size();
+            for (int i=0; i<fullGraphsListSize; i++) {
+                System.out.print("Węzły grafu " + (i + 1) + ": ");
+                for (Integer cell : fullGraphsList.get(i)) {
+                    System.out.print(String.format("SDG%d, ", cell + 1));
+                }
+                System.out.print("\b\b\n");
             }
-            System.out.print("\n");
+        }
 
-        }*/
+
+
+
+
     }
+
+
 }
